64ab03e3f955faf47416b0038922502e6290d5fb
vasko-de-gama <v.nikitenko@list.ru>
2019-04-08 09:36:41 +0300

    'author' default role

--- api/perl/FB3-Convert/lib/FB3/Convert/Epub.pm
+++ api/perl/FB3-Convert/lib/FB3/Convert/Epub.pm
@@ -1600,7 +1600,7 @@ sub FB3Creator {
     my $First  = FB3::Convert::xmlescape($_->{'first-name'} );
     my $Middle = FB3::Convert::xmlescape($_->{'middle-name'});
     my $Last   = FB3::Convert::xmlescape($_->{'last-name'}  ) || "Unknown";
-    my $Link   = FB3::Convert::xmlescape($_->{'link'}) || "undef";
+    my $Link   = FB3::Convert::xmlescape($_->{'link'}) || "author";
 
     print FHdesc qq{<subject link="$Link" id="}.($_->{'id'}||'00000000-0000-0000-0000-000000000000').qq{">
       <title>
